9dc30480074957131468d6a4c15129f2a6882096
Muhammad Fajar Alfad <112568404+fajaralfad@users.noreply.github.com>
2024-06-13 22:44:14 +0700

    Add files via upload

---
+++ Sistem rekomendasi Kuliah/UAP.pl
@@ -0,0 +1,182 @@
+% Basis Pengetahuan untuk Jurusan dan Bakat/Minat yang Terkait (65 jurusan)
+jurusan(agroteknologi, [(biologi, 4), (kimia, 3), (teknologi_pertanian, 2), (ekologi, 3)]).
+jurusan(agribisnis, [(manajemen, 3), (pemasaran, 4), (pertanian, 2), (ekonomi, 3)]).
+jurusan(teknik_geofisika, [(fisika, 4), (matematika, 3), (geologi, 2), (pemecahan_masalah, 3)]).
+jurusan(teknik_sipil, [(fisika, 4), (matematika, 3), (desain, 3), (manajemen_proyek, 4)]).
+jurusan(kimia, [(kimia, 4), (penelitian, 3), (lab_works, 4), (matematika, 2)]).
+jurusan(biologi, [(biologi, 4), (penelitian, 3), (ekologi, 2), (lab_works, 3)]).
+jurusan(teknologi_hasil_pertanian, [(teknologi_pertanian, 4), (biologi, 3), (kimia, 2), (pangan, 3)]).
+jurusan(peternakan, [(biologi, 4), (manajemen_peternakan, 3), (kesehatan_hewan, 2), (penelitian, 3)]).
+jurusan(kehutanan, [(biologi, 4), (ekologi, 3), (konservasi, 4), (teknologi_pertanian, 2)]).
+jurusan(teknik_pertanian, [(teknologi_pertanian, 4), (fisika, 3), (manajemen, 2), (biologi, 2)]).
+jurusan(matematika, [(matematika, 4), (logika, 3), (pemecahan_masalah, 4), (penelitian, 2)]).
+jurusan(fisika, [(fisika, 4), (matematika, 3), (penelitian, 3), (teknologi, 2)]).
+jurusan(teknik_mesin, [(fisika, 4), (matematika, 3), (desain_mesin, 4), (teknologi, 3)]).
+jurusan(teknik_elektro, [(elektronika, 4), (fisika, 3), (matematika, 2), (teknologi, 3)]).
+jurusan(pendidikan_matematika, [(matematika, 4), (mengajar, 3), (logika, 2), (penelitian, 2)]).
+jurusan(pendidikan_fisika, [(fisika, 4), (mengajar, 3), (penelitian, 2), (lab_works, 2)]).
+jurusan(pendidikan_kimia, [(kimia, 4), (mengajar, 3), (penelitian, 2), (lab_works, 2)]).
+jurusan(pendidikan_biologi, [(biologi, 4), (mengajar, 3), (penelitian, 2), (lab_works, 2)]).
+jurusan(teknik_kimia, [(kimia, 4), (matematika, 3), (penelitian, 3), (teknologi, 2)]).
+jurusan(pendidikan_kedokteran, [(biologi, 4), (kimia, 3), (empati, 4), (penelitian, 2)]).
+jurusan(budidaya_perairan, [(biologi, 4), (ekologi_perairan, 3), (manajemen_perikanan, 2), (penelitian, 3)]).
+jurusan(ilmu_komputer, [(pemrograman, 4), (logika, 3), (matematika, 2), (teknologi, 3)]).
+jurusan(arsitektur, [(desain, 4), (matematika, 2), (fisika, 2), (manajemen_proyek, 3)]).
+jurusan(teknik_geodesi, [(matematika, 3), (geografi, 3), (teknologi, 2), (pemecahan_masalah, 3)]).
+jurusan(teknik_informatika, [(pemrograman, 4), (matematika, 2), (logika, 3), (teknologi, 3)]).
+jurusan(sumberdaya_akuatik, [(biologi, 3), (ekologi_perairan, 3), (penelitian, 2), (manajemen_perikanan, 2)]).
+jurusan(ilmu_tanah, [(biologi, 3), (kimia, 3), (teknologi_pertanian, 2), (penelitian, 2)]).
+jurusan(agronomi, [(biologi, 4), (ekologi, 3), (teknologi_pertanian, 2), (penelitian, 2)]).
+jurusan(proteksi_tanaman, [(biologi, 4), (ekologi, 3), (penelitian, 2), (manajemen_hama, 3)]).
+jurusan(penyuluhan_pertanian, [(komunikasi, 4), (biologi, 2), (manajemen, 3), (teknologi_pertanian, 2)]).
+jurusan(ilmu_kelautan, [(biologi, 4), (ekologi_perairan, 3), (konservasi, 3), (penelitian, 2)]).
+jurusan(teknologi_industri_pertanian, [(teknologi_pertanian, 4), (manajemen, 3), (biologi, 2), (penelitian, 2)]).
+jurusan(nutrisi_dan_teknologi_pakan_ternak, [(biologi, 4), (manajemen_pakan, 3), (kesehatan_hewan, 2), (penelitian, 2)]).
+jurusan(pendidikan_teknologi_informasi, [(pemrograman, 4), (mengajar, 3), (logika, 2), (teknologi, 2)]).
+jurusan(farmasi, [(kimia, 4), (biologi, 3), (penelitian, 3), (manajemen, 2)]).
+jurusan(biologi_terapan, [(biologi, 4), (penelitian, 3), (lab_works, 3), (teknologi, 2)]).
+jurusan(teknik_lingkungan, [(biologi, 3), (kimia, 2), (konservasi, 4), (penelitian, 2)]).
+jurusan(hukum, [(berpikir_kritis, 4), (berbicara_di_depan_umum, 3), (menulis, 3), (argumentasi, 3)]).
+jurusan(sosiologi, [(komunikasi, 4), (penelitian, 3), (berpikir_kritis, 3), (analisis_sosial, 3)]).
+jurusan(ilmu_pemerintahan, [(komunikasi, 4), (penelitian, 3), (manajemen, 3), (politik, 2)]).
+jurusan(manajemen, [(kepemimpinan, 4), (pemasaran, 3), (keuangan, 3), (strategi_bisnis, 3)]).
+jurusan(iesp, [(ekonomi, 4), (penelitian, 3), (manajemen, 3), (statistik, 2)]).
+jurusan(akuntansi, [(matematika, 4), (keuangan, 3), (manajemen, 2), (analisis_data, 2)]).
+jurusan(ilmu_komunikasi, [(komunikasi, 4), (penelitian, 3), (penulisan, 3), (jurnalisme, 2)]).
+jurusan(ilmu_adm_negara, [(komunikasi, 4), (manajemen, 3), (politik, 3), (penelitian, 2)]).
+jurusan(ilmu_adm_niaga, [(komunikasi, 4), (manajemen, 3), (pemasaran, 3), (keuangan, 2)]).
+jurusan(ppkn, [(komunikasi, 4), (politik, 3), (mengajar, 2), (penelitian, 2)]).
+jurusan(pend_sejarah, [(komunikasi, 4), (mengajar, 3), (penelitian, 2), (penulisan, 2)]).
+jurusan(pend_geografi, [(geografi, 4), (mengajar, 3), (penelitian, 2), (penulisan, 2)]).
+jurusan(pend_ekonomi, [(ekonomi, 4), (mengajar, 3), (penelitian, 2), (matematika, 2)]).
+jurusan(pendidikan_jasmani, [(olahraga, 4), (mengajar, 3), (biologi, 2), (psikologi, 2)]).
+jurusan(bimbingan_konseling, [(empati, 4), (komunikasi, 3), (psikologi, 3), (mengajar, 2)]).
+jurusan(pend_bahasa_indonesia, [(menulis, 4), (mengajar, 3), (sastra, 2), (penelitian, 2)]).
+jurusan(pend_b_inggris, [(menulis, 4), (mengajar, 3), (sastra, 2), (penelitian, 2)]).
+jurusan(pgsd, [(mengajar, 4), (komunikasi, 3), (perkembangan_anak, 3), (penelitian, 2)]).
+jurusan(pendidikan_tari, [(tari, 4), (mengajar, 3), (seni, 2), (penelitian, 2)]).
+jurusan(pendidikan_anak_usia_dini, [(mengajar, 4), (komunikasi, 3), (perkembangan_anak, 3), (psikologi, 2)]).
+jurusan(hubungan_internasional, [(komunikasi, 4), (politik, 3), (penelitian, 2), (bahasa_asing, 2)]).
+jurusan(bahasa_perancis, [(menulis, 4), (mengajar, 3), (bahasa_asing, 2), (sastra, 2)]).
+jurusan(pendidikan_musik, [(musik, 4), (mengajar, 3), (seni, 2), (penelitian, 2)]).
+jurusan(pendidikan_bahasa_lampung, [(menulis, 4), (mengajar, 3), (sastra, 2), (penelitian, 2)]).
+jurusan(bisnis_digital, [(teknologi, 4), (pemasaran, 3), (ekonomi, 2), (manajemen, 2)]).
+jurusan(sistem_informasi, [(pemrograman, 4), (manajemen, 3), (logika, 2), (teknologi, 2)]).
+jurusan(teknik_geologi, [(geologi, 4), (matematika, 3), (fisika, 2), (penelitian, 2)]).
+jurusan(teknologi_rekayasa_otomotif, [(otomotif, 4), (fisika, 3), (matematika, 2), (teknologi, 2)]).
+
+% Meminta pengguna tentang bakat dan minat mereka, dengan validasi input
+tanya_pertanyaan(Pertanyaan, TingkatKeyakinan) :-
+    format('~w (0: Tidak Yakin, 1: Kurang Yakin, 2: Cukup Yakin, 3: Yakin, 4: Sangat Yakin): ', [Pertanyaan]),
+    nl,
+    read(Input),
+    validasi_input(Input, TingkatKeyakinan).
+
+% Memvalidasi input agar berada dalam rentang 0-4
+validasi_input(Input, TingkatKeyakinan) :-
+    integer(Input),
+    between(0, 4, Input), !,
+    TingkatKeyakinan = Input.
+validasi_input(_, TingkatKeyakinan) :-
+    writeln('Input tidak valid. Masukkan angka antara 0 dan 4.'),
+    read(InputBaru),
+    validasi_input(InputBaru, TingkatKeyakinan).
+
+pertanyaan([
+    ('Apakah Anda suka memecahkan masalah atau mencari solusi untuk tantangan yang kompleks?', memecahkan_masalah),
+    ('Apakah Anda menikmati matematika dan menemukan kepuasan dalam menganalisis angka dan pola?', matematika),
+    ('Apakah Anda tertarik pada teknologi dan bagaimana inovasi teknologi dapat mengubah cara kita bekerja dan hidup?', teknologi),
+    ('Apakah Anda memiliki kemampuan pemrograman atau pengalaman dalam mengembangkan kode untuk membuat program atau aplikasi?', pemrograman),
+    ('Apakah Anda memiliki kemampuan komunikasi yang baik, baik dalam menyampaikan ide-ide kompleks atau mendengarkan dengan empati?', komunikasi),
+    ('Apakah Anda memiliki empati yang tinggi dan mampu memahami dan merespons perasaan orang lain dengan baik?', empati),
+    ('Apakah Anda suka melakukan penelitian untuk mendalami topik tertentu atau menemukan informasi baru?', penelitian),
+    ('Apakah Anda suka berpikir kritis dan mengevaluasi informasi dengan cermat sebelum membuat keputusan atau mengambil tindakan?', berpikir_kritis),
+    ('Apakah Anda suka memberikan konseling atau bantuan kepada orang lain untuk membantu mereka mengatasi masalah atau mencapai tujuan mereka?', konseling),
+    ('Apakah Anda memiliki kemampuan kepemimpinan dan mampu memotivasi dan mengarahkan tim atau kelompok dalam mencapai tujuan bersama?', kepemimpinan),
+    ('Apakah Anda suka berpikir strategis untuk merencanakan langkah-langkah jangka panjang dan mencapai hasil yang diinginkan?', berpikir_strategis),
+    ('Apakah Anda tertarik pada keuangan dan bagaimana mengelola uang serta investasi secara cerdas?', keuangan),
+    ('Apakah Anda tertarik pada pemasaran dan bagaimana strategi pemasaran dapat memengaruhi persepsi dan keputusan konsumen?', pemasaran),
+    ('Apakah Anda kreatif dan suka mengekspresikan ide-ide baru melalui seni atau inovasi?', kreativitas),
+    ('Apakah Anda suka seni visual dan menikmati menciptakan atau mengapresiasi karya seni dalam bentuk visual?', seni_visual),
+    ('Apakah Anda suka desain dan bagaimana desain mempengaruhi fungsi dan estetika produk atau lingkungan?', desain),
+    ('Apakah Anda tertarik pada sejarah seni dan bagaimana perkembangan budaya mempengaruhi karya seni dari masa ke masa?', sejarah_seni),
+    ('Apakah Anda suka fisika dan memiliki ketertarikan dalam memahami hukum alam dan fenomena fisika di sekitar kita?', fisika),
+    ('Apakah Anda memiliki keterampilan teknis dalam bidang tertentu yang memungkinkan Anda mengaplikasikan pengetahuan teknis dalam situasi praktis?', keterampilan_teknis),
+    ('Apakah Anda memiliki kemampuan manajemen proyek untuk mengatur waktu, sumber daya, dan orang dalam proyek dengan efektif?', manajemen_proyek),
+    ('Apakah Anda tertarik pada biologi dan bagaimana kehidupan berkembang dan beradaptasi di berbagai lingkungan?', biologi),
+    ('Apakah Anda tertarik pada kimia dan bagaimana bahan-bahan berinteraksi dan bereaksi satu sama lain dalam berbagai konteks?', kimia),
+    ('Apakah Anda memiliki kemampuan berbicara di depan umum dan merasa nyaman berkomunikasi dengan audiens yang beragam?', berbicara_di_depan_umum),
+    ('Apakah Anda suka menulis dan mengekspresikan ide atau informasi dengan jelas dan persuasif dalam bentuk tulisan?', menulis),
+    ('Apakah Anda tertarik pada argumentasi dan debat untuk menyelidiki berbagai sudut pandang dalam sebuah argumen atau masalah?', argumentasi),
+    ('Apakah Anda suka mewawancarai orang untuk mendapatkan wawasan mendalam tentang pengalaman dan perspektif mereka?', wawancara),
+    ('Apakah Anda tertarik pada pendidikan anak dan bagaimana mengembangkan potensi anak-anak melalui pendidikan?', perkembangan_anak),
+    ('Apakah Anda tertarik pada studi lingkungan dan bagaimana manusia berinteraksi dengan lingkungan alam mereka?', studi_lingkungan),
+    ('Apakah Anda suka melakukan konservasi untuk melestarikan keanekaragaman hayati dan sumber daya alam?', konservasi),
+    ('Apakah Anda tertarik pada ekonomi dan bagaimana keputusan ekonomi mempengaruhi individu dan masyarakat secara luas?', ekonomi),
+    ('Apakah Anda suka bekerja di laboratorium untuk melakukan eksperimen dan mengeksplorasi fenomena ilmiah?', lab_works),
+    ('Apakah Anda tertarik pada geologi dan bagaimana proses geologis membentuk planet kita?', geologi),
+    ('Apakah Anda memiliki keterampilan dalam teknologi pertanian untuk meningkatkan produktivitas dan keberlanjutan pertanian?', teknologi_pertanian),
+    ('Apakah Anda tertarik pada ekologi dan hubungan kompleks antara organisme hidup dan lingkungannya?', ekologi),
+    ('Apakah Anda tertarik pada kesehatan hewan dan kesejahteraan hewan dalam berbagai konteks?', kesehatan_hewan),
+    ('Apakah Anda suka melakukan pemetaan atau survei untuk mengumpulkan data dan membuat representasi visual dari informasi?', pemetaan),
+    ('Apakah Anda tertarik pada studi kelautan dan bagaimana kehidupan di lautan berinteraksi dan beradaptasi?', studi_kelautan),
+    ('Apakah Anda tertarik pada agribisnis dan bagaimana produksi makanan dan sumber daya alam dikelola secara efisien?', agribisnis),
+    ('Apakah Anda tertarik pada sosiologi dan bagaimana struktur sosial dan budaya mempengaruhi perilaku manusia?', sosiologi),
+    ('Apakah Anda tertarik pada politik dan bagaimana kekuasaan dan keputusan politik memengaruhi masyarakat?', politik),
+    ('Apakah Anda suka berolahraga untuk menjaga kesehatan fisik dan mental Anda?', olahraga),
+    ('Apakah Anda suka menari untuk mengekspresikan diri dan menikmati ritme dan gerakan?', tari),
+    ('Apakah Anda tertarik pada musik dan bagaimana musik menginspirasi emosi dan mempengaruhi budaya?', musik),
+    ('Apakah Anda tertarik pada otomotif dan bagaimana teknologi otomotif mengubah cara kita bergerak?', otomotif),
+    ('Apakah Anda suka melakukan eksperimen untuk menguji hipotesis dan menemukan solusi inovatif untuk masalah?', eksperimen),
+    ('Apakah Anda tertarik pada bisnis digital dan bagaimana teknologi mengubah cara bisnis dijalankan dan berinteraksi dengan pelanggan?', bisnis_digital),
+    ('Apakah Anda tertarik pada hukum dan bagaimana sistem hukum mengatur dan mempengaruhi masyarakat?', hukum),
+    ('Apakah Anda suka mengajar untuk berbagi pengetahuan dan mempengaruhi perkembangan intelektual orang lain?', mengajar),
+    ('Apakah Anda tertarik pada psikologi dan bagaimana pikiran dan perilaku manusia dipengaruhi oleh faktor psikologis?', psikologi)
+]).
+
+% Menghitung tingkat keyakinan untuk setiap bakat/minat dengan mempertimbangkan bobot
+hitung_keyakinan(_, [], 0).
+hitung_keyakinan(JawabanPertanyaan, [(Bakat, Bobot)|Sisa], Keyakinan) :-
+    (member((Bakat, Tingkat), JawabanPertanyaan) -> TingkatBobot is Tingkat * Bobot ; TingkatBobot is 0),
+    hitung_keyakinan(JawabanPertanyaan, Sisa, KeyakinanSisa),
+    Keyakinan is TingkatBobot + KeyakinanSisa.
+
+% Menentukan jurusan terbaik berdasarkan tingkat keyakinan gabungan
+rekomendasikan_jurusan(JawabanPertanyaan, JurusanDirekomendasikan, AlasanDirekomendasikan) :-
+    findall((Jurusan, Keyakinan), (
+        jurusan(Jurusan, BakatBobot),
+        hitung_keyakinan(JawabanPertanyaan, BakatBobot, Keyakinan)
+    ), KeyakinanJurusan),
+    sort(2, @>=, KeyakinanJurusan, DaftarKeyakinanJurusan),
+    DaftarKeyakinanJurusan = [(_, KeyakinanTertinggi)|_],
+    findall(Jurusan, member((Jurusan, KeyakinanTertinggi), DaftarKeyakinanJurusan), JurusanDirekomendasikan),
+    findall((Jurusan, Alasan), (
+        member(Jurusan, JurusanDirekomendasikan),
+        jurusan(Jurusan, BakatBobot),
+        findall(B, member((B, _), BakatBobot), Alasan)
+    ), AlasanDirekomendasikan).
+
+% Fungsi utama untuk menjalankan sistem rekomendasi
+rekomendasi_jurusan_kuliah :-
+    pertanyaan(PertanyaanList),
+    findall((Bakat, TingkatKeyakinan), (
+        member((Pertanyaan, Bakat), PertanyaanList),
+        tanya_pertanyaan(Pertanyaan, TingkatKeyakinan)
+    ), JawabanPertanyaan),
+    rekomendasikan_jurusan(JawabanPertanyaan, JurusanDirekomendasikan, AlasanDirekomendasikan),
+    nl, writeln('Jurusan yang direkomendasikan untuk Anda adalah:'),
+    tulis_rekomendasi(JurusanDirekomendasikan, AlasanDirekomendasikan).
+
+% Menulis daftar jurusan yang direkomendasikan dan alasan-alasan yang sesuai
+tulis_rekomendasi([], []).
+tulis_rekomendasi([Jurusan|TJurusan], [(Jurusan, Alasan)|TAlasan]) :-
+    format('- ~w', [Jurusan]), nl,
+    writeln('  Alasan:'),
+    tulis_alasan(Alasan),
+    tulis_rekomendasi(TJurusan, TAlasan).
+
+% Menulis alasan-alasan yang sesuai dengan jurusan yang direkomendasikan
+tulis_alasan([]).
+tulis_alasan([H|T]) :-
+    format('  - ~w', [H]), nl,
+    tulis_alasan(T).
